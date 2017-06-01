ee3b25c6cf054794840cc1b493636e23dfe8a71c
Cam Beaudoin <camilebeaudoin@gmail.com>
2017-06-01 13:55:55 -0400

    working on angular filter

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var app = angular.module('characterBuilder', []);
+var app = angular.module('characterBuilder', ['ngAria']);
 
 app.controller('MainController', function ($scope, $http) {
   $http.get('assets/content.json')
@@ -8,4 +8,50 @@ app.controller('MainController', function ($scope, $http) {
         $scope.content.push(res.data[i]);
       }
     });
+
+  $scope.sortBy = function (columnName) {
+    $scope.reverse = ($scope.columnName === columnName) ? !$scope.reverse : false;
+    $scope.columnName = columnName;
+  }
+
+  // $scope.search = {};
+
+  // $scope.filterFunction = function (elem) {
+  //   console.log(elem);
+  //   return ($scope.search.lvlLow ? elem.lvlLow === $scope.search.lvlLow : elem) &&
+  //          ($scope.search.lvlHigh ? elem.lvlHigh === $scope.search.lvlHigh : elem) &&
+  //          ($scope.search.title ? angular.lowercase(elem.title).indexOf(angular.lowercase($scope.search.title) || '') : elem)
+  //   // return elem;
+  // }
+
+})
+
+app.directive('sortArrow', [function () {
+  return {
+    restrict: 'A',
+    template: '<i class="fa" ng-class="{\'fa-arrow-up\': reverse, \'fa-arrow-down\': !reverse}" aria-hidden="true"></i>',
+    link: function (scope, elem) {
+      // console.log(scope, elem);
+    }
+  }
+}])
+
+app.filter('contentFilter', function () {
+  return function (items, toFilter) {
+    if (items) {
+      if (toFilter) {
+        var keys = Object.keys(toFilter);
+        var filtered = [];
+        keys.forEach(function (key) {
+          if (toFilter[key]) {
+            filtered = items.filter(function (item) {
+              return (toFilter[key] === item[key])
+            })
+          }
+        })
+        return filtered;
+      }
+      return items;
+    }
+  }
 })
